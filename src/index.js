6970c831d4e2d728f26e0e0f24d05107717bd514
Taesuuu <taesu08@gmail.com>
2021-12-27 17:23:42 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const formatName = function (name) {
+  return name.firstName + " " + name.lastName;
+};
+const name = {
+  firstName: "hi",
+  lastName: "haaa",
+};
+const elemet = <h1>Hello, {formatName(name)}!</h1>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  elemet,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
